d6f19033764a8864b79325b1aaa6df4fc958a8ac
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-24 15:27:02 +0300

    feat(header): add responsive mobile drawer navigation

---
+++ src/Header/DrawerComp.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import {
+  Drawer,
+  ListItemButton,
+  ListItemText,
+  List,
+  IconButton,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const DrawerComp = ({ links }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Drawer open={open} onClose={() => setOpen(false)}>
+        <List>
+          {links.map((item, i) => (
+            <ListItemButton key={i}>
+              <ListItemText>{item}</ListItemText>
+            </ListItemButton>
+          ))}
+        </List>
+      </Drawer>
+      <IconButton sx={{ ml: "auto" }} onClick={() => setOpen(true)}>
+        <MenuIcon color="white" />
+      </IconButton>
+    </>
+  );
+};
+
+export default DrawerComp;
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -9,51 +9,69 @@ import {
   Button,
   Link,
   Typography,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import ApiIcon from "@mui/icons-material/Api";
+import DrawerComp from "./DrawerComp";
 
 const links = ["Products", "Solutions", "Pricing", "Enterprise"];
 
 const Header = () => {
+  const theme = useTheme();
+  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <AppBar sx={{ bgcolor: "transparent", boxShadow: 0, position: "sticky" }}>
-      <Toolbar>
-        <Box
-          sx={{
-            display: "flex",
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <ApiIcon sx={{ color: "black" }} />
-            <Tabs component={Link} sx={{ textDecoration: "none" }}>
-              {links.map((link, i) => {
-                return (
-                  <Tab
-                    key={i}
-                    label={link}
-                    sx={{
-                      fontWeight: "bold",
-                      textDecoration: "none",
-                      ":hover": {
-                        textDecoration: "underline",
-
-                        textUnderlineOffset: "5px",
-                      },
-                    }}
-                  ></Tab>
-                );
-              })}
-            </Tabs>
-          </Box>
-          <Box sx={{ display: "flex", gap: 2 }}>
-            <Button variant="outlined">Talk To Us</Button>
-            <Button variant="contained">Try For Free</Button>
-          </Box>
+      {isMatch ? (
+        <Box sx={{ display: "flex" }}>
+          <ApiIcon sx={{ color: "black", padding: 1 }} />
+          <Typography
+            variant="h6"
+            sx={{ fontFamily: "fantasy", color: "black", padding: 0.5 }}
+          >
+            CodeEnv
+          </Typography>
+          <DrawerComp links={links} />
         </Box>
-      </Toolbar>
+      ) : (
+        <Toolbar>
+          <Box
+            sx={{
+              display: "flex",
+              width: "100%",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <ApiIcon sx={{ color: "black" }} />
+              <Tabs component={Link} sx={{ textDecoration: "none" }}>
+                {links.map((link, i) => {
+                  return (
+                    <Tab
+                      key={i}
+                      label={link}
+                      sx={{
+                        fontWeight: "bold",
+                        textDecoration: "none",
+                        ":hover": {
+                          textDecoration: "underline",
+
+                          textUnderlineOffset: "5px",
+                        },
+                      }}
+                    ></Tab>
+                  );
+                })}
+              </Tabs>
+            </Box>
+            <Box sx={{ display: "flex", gap: 2 }}>
+              <Button variant="outlined">Talk To Us</Button>
+              <Button variant="contained">Try For Free</Button>
+            </Box>
+          </Box>
+        </Toolbar>
+      )}
       <Box sx={{ width: "100%", height: "100vh" }}>
         <video
           width={"100%"}
