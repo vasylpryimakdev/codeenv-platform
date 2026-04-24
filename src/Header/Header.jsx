53d7c71a16422b8e96658e5b1f5d2226a722384b
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-24 13:35:35 +0300

    feat(header): add navigation tabs and action buttons

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,14 +1,51 @@
 import React from "react";
 
-import { AppBar, Toolbar, Box } from "@mui/material";
+import { AppBar, Toolbar, Box, Tabs, Tab, Button, Link } from "@mui/material";
 import ApiIcon from "@mui/icons-material/Api";
+
+const links = ["Products", "Solutions", "Pricing", "Enterprise"];
 
 const Header = () => {
   return (
     <AppBar sx={{ bgcolor: "transparent", boxShadow: 0 }}>
       <Toolbar>
-        <Box>
+        <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            alignItems: "center",
+          }}
+        >
           <ApiIcon sx={{ color: "black" }} />
+          <Box>
+            <Tabs component={Link} sx={{ textDecoration: "none" }}>
+              {links.map((link, i) => {
+                return (
+                  <Tab
+                    key={i}
+                    label={link}
+                    sx={{
+                      fontWeight: "bold",
+                      textDecoration: "none",
+                      ":hover": {
+                        textDecoration: "underline",
+
+                        textUnderlineOffset: "5px",
+                      },
+                    }}
+                  ></Tab>
+                );
+              })}
+            </Tabs>
+          </Box>
+          <Box sx={{ display: "flex", marginLeft: "auto" }}>
+            <Button sx={{ mr: 2 }} variant="outlined">
+              Talk To Us
+            </Button>
+            <Button sx={{ ml: 2 }} variant="contained">
+              Try For Free
+            </Button>
+          </Box>
         </Box>
       </Toolbar>
     </AppBar>
